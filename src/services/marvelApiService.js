cdcc43c4b24b81e79c1c80401c18f31c2d38b32d
mrcal56 <marlon56r@gmail.com>
2025-07-22 09:36:41 -0600

    depuración

--- src/services/marvelApiService.js
+++ src/services/marvelApiService.js
@@ -3,6 +3,10 @@ import md5 from "md5";
 
 const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
 const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
+
+console.log("🔍 MARVEL PUBLIC KEY:", publicKey);
+console.log("🛡️ MARVEL PRIVATE KEY:", privateKey);
+
 const baseURL = 'https://gateway.marvel.com/v1/public/characters'
 
 const MarvelApiService = {
